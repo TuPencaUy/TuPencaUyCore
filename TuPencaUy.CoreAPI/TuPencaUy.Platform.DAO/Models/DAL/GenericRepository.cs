fa370ef38cedef088ed71456dd66b3e3f3124892
Gastón <gastonsomac@gmail.com>
2024-04-28 22:38:03 -0300

    Fixed applying conditions in the generic repository.

--- TuPencaUy.CoreAPI/TuPencaUy.Platform.DAO/Models/DAL/GenericRepository.cs
+++ TuPencaUy.CoreAPI/TuPencaUy.Platform.DAO/Models/DAL/GenericRepository.cs
@@ -17,90 +17,97 @@ namespace TuPencaUy.Platform.DAO.Models.Logic
     internal PlatformDbContext _context;
     internal DbSet<TEntity> _dbSet;
 
-    public IQueryable<TEntity> Get(
-      List<Expression<Func<TEntity, bool>>>? conditions = null,
-      Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
-      EnumDataStatus dataStatus = EnumDataStatus.Active)
+    public GenericRepository(PlatformDbContext context)
     {
-      var query = FilterRegistersByStatus(dataStatus);
-
-      if(conditions is not null)
-      {
-        foreach(Expression<Func<TEntity, bool>> condition in conditions)
-        {
-          query.Where(condition);
-        }
-      }
-
-      return orderBy is not null ? orderBy(query) : query;
+      _context = context;
+      _context.Database.SetCommandTimeout(3600);
+      _dbSet = _context.Set<TEntity>();
     }
 
-    public void Insert(TEntity entity)
-    {
-      if(entity is ControlDate controlDateEntity)
-      {
-        controlDateEntity.CreationDate = DateTime.Now;
-        controlDateEntity.LastModificationDate = DateTime.Now;
-      }
+  public IQueryable<TEntity> Get(
+    List<Expression<Func<TEntity, bool>>>? conditions = null,
+    Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+    EnumDataStatus dataStatus = EnumDataStatus.Active)
+  {
+    var query = FilterRegistersByStatus(dataStatus);
 
-      _dbSet.Add(entity);
+    if (conditions is not null)
+    {
+      foreach (Expression<Func<TEntity, bool>> condition in conditions)
+      {
+        query = query.Where(condition);
+      }
     }
 
-    public void Delete(object id)
-    {
-      TEntity entityToDelete = _dbSet.Find(id);
-      this.Delete(entityToDelete);
-    }
-    public void Delete(TEntity entity)
-    {
-      if (_context.Entry(entity).State == EntityState.Detached)
-      {
-        _dbSet.Attach(entity);
-      }
+    return orderBy is not null ? orderBy(query) : query;
+  }
 
-      _dbSet.Remove(entity);
-    }
-    public void Update(TEntity entity)
+  public void Insert(TEntity entity)
+  {
+    if (entity is ControlDate controlDateEntity)
     {
-      if (entity is ControlDate controlDateEntity)
-      {
-        controlDateEntity.LastModificationDate = DateTime.Now;
-      }
+      controlDateEntity.CreationDate = DateTime.Now;
+      controlDateEntity.LastModificationDate = DateTime.Now;
+    }
 
+    _dbSet.Add(entity);
+  }
+
+  public void Delete(object id)
+  {
+    TEntity entityToDelete = _dbSet.Find(id);
+    this.Delete(entityToDelete);
+  }
+  public void Delete(TEntity entity)
+  {
+    if (_context.Entry(entity).State == EntityState.Detached)
+    {
       _dbSet.Attach(entity);
-      _context.Entry(entity).State = EntityState.Modified;
     }
 
-    public void SaveChanges()
+    _dbSet.Remove(entity);
+  }
+  public void Update(TEntity entity)
+  {
+    if (entity is ControlDate controlDateEntity)
     {
-      try
-      {
-        _context.SaveChanges();
-      }
-      catch (Exception)
-      {
-        throw;
-      }
+      controlDateEntity.LastModificationDate = DateTime.Now;
     }
 
-    private IQueryable<TEntity> FilterRegistersByStatus(EnumDataStatus status)
+    _dbSet.Attach(entity);
+    _context.Entry(entity).State = EntityState.Modified;
+  }
+
+  public void SaveChanges()
+  {
+    try
     {
-      IQueryable<TEntity> query;
-
-      switch(status)
-      {
-        case EnumDataStatus.Active:
-          query = _dbSet.Where(q => !q.Inactive);
-          break;
-        case EnumDataStatus.Inactive:
-          query = _dbSet.Where(q => q.Inactive);
-          break;
-        default:
-          query = _dbSet;
-          break;
-      }
-
-      return query;
+      _context.SaveChanges();
+    }
+    catch (Exception)
+    {
+      throw;
     }
   }
+
+  private IQueryable<TEntity> FilterRegistersByStatus(EnumDataStatus status)
+  {
+    IQueryable<TEntity> query;
+
+    switch (status)
+    {
+      case EnumDataStatus.Active:
+        query = _dbSet.Where(q => !q.Inactive);
+        break;
+      case EnumDataStatus.Inactive:
+        query = _dbSet.Where(q => q.Inactive);
+        break;
+      default:
+        query = _dbSet;
+        break;
+    }
+
+    return query;
+  }
+}
 }
