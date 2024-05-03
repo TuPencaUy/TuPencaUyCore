c7d979bfdcb551e3c2d649556d52594ee8488009
Gastón <gastonsomac@gmail.com>
2024-05-02 22:08:16 -0300

    Add a project of data access logic for repositories.

---
+++ TuPencaUy.CoreAPI/TuPencaUy.Core.DataAccessLogic/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+using TuPencaUy.Platform.DAO;
+
+namespace TuPencaUy.Core.DataAccessLogic
+{
+  public interface IGenericRepository<TEntity> where TEntity : class
+  {
+    IQueryable<TEntity> Get(
+      List<Expression<Func<TEntity, bool>>>? conditions = null,
+      Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+      EnumDataStatus dataStatus = EnumDataStatus.Active);
+
+    void Insert(TEntity entity);
+    void Delete(object id);
+    void Delete(TEntity entity);
+    void Update(TEntity entity);
+    void SaveChanges();
+  }
+}
