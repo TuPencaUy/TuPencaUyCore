b7d99b19ccd0b3945d5ef2978464c313a92fa31c
Gastón <gastonsomac@gmail.com>
2024-05-06 09:59:50 -0300

    New site service

---
+++ TuPencaUy.CoreAPI/TuPencaUy.Platform.DataServices/Services/ISiteService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuPencaUy.Core.DTOs;
+
+namespace TuPencaUy.Core.DataServices.Services
+{
+  public interface ISiteService
+  {
+    SiteDTO GetSiteByDomain(string domain);
+  }
+}
