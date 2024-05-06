99d8a6841b62f80b645ee0e433185774030a9dc7
Gastón <gastonsomac@gmail.com>
2024-05-06 09:56:52 -0300

    New Site data access objects

---
+++ TuPencaUy.CoreAPI/TuPencaUy.Site.DAO/Models/Data/SiteDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TuPencaUy.Site.DAO.Models.Data
+{
+  public class SiteDbContext : DbContext
+  {
+    public SiteDbContext(DbContextOptions<SiteDbContext> options) : base(options)
+    {
+    }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Permission> Permissions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<User>().ToTable("User");
+      modelBuilder.Entity<Role>().ToTable("Role");
+      modelBuilder.Entity<Permission>().ToTable("Permission");
+    }
+  }
+}
---
+++ TuPencaUy.CoreAPI/TuPencaUy.Site.DAO/Models/User.cs
@@ -0,0 +1,31 @@
+﻿namespace TuPencaUy.Site.DAO.Models
+{
+  using System.ComponentModel.DataAnnotations;
+  using System.ComponentModel.DataAnnotations.Schema;
+  using TuPencaUy.Site.DAO.Models.Base;
+
+  public class User : ControlDate
+  {
+    [Column("Id", Order = 0)]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [MaxLength(50)]
+    [Column("Name", Order = 1, TypeName = "varchar")]
+    public required string Name { get; set; }
+
+    [Key]
+    [MaxLength(50)]
+    [Column("Email", Order = 2, TypeName = "varchar")]
+    public string? Email { get; set; }
+
+    [MaxLength(100)]
+    [Column("Password", Order = 3, TypeName = "varchar")]
+    public string? Password { get; set; }
+
+    [ForeignKey("Role")]
+    [Column("RoleId", Order = 4)]
+    public int? roleId { get; set; }
+    public virtual Role? Role { get; set; }
+  }
+}
