7e1fe633217fa8b6f075246a2d5a07f7465365df
Gastón <gastonsomac@gmail.com>
2024-05-06 09:57:59 -0300

    New exceptions: InvalidCredentials, SiteNotFound

---
+++ TuPencaUy.CoreAPI/TuPencaUy.Exceptions/InvalidCredentialsException.cs
@@ -0,0 +1,8 @@
+﻿namespace TuPencaUy.Exceptions
+{
+  public class InvalidCredentialsException : Exception
+  {
+    public InvalidCredentialsException() : base(message: "Invalid credentials") { }
+    public InvalidCredentialsException(string message) : base(message: message) { }
+  }
+}
---
+++ TuPencaUy.CoreAPI/TuPencaUy.Exceptions/SiteNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuPencaUy.Core.Exceptions
+{
+  public class SiteNotFoundException : Exception
+  {
+    public SiteNotFoundException() : base(message: "Site not found") { }
+    public SiteNotFoundException(string message) : base(message: message) { }
+  }
+}
