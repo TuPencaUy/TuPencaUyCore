3f0be497c49b34ede3f29ba24a255e8b90b4942c
Gastón <gastonsomac@gmail.com>
2024-06-03 16:51:49 -0300

    Services layer

--- TuPencaUy.CoreAPI/TuPencaUy.Core.DataServices/Services/Platform/PlatformEventService.cs
+++ TuPencaUy.CoreAPI/TuPencaUy.Core.DataServices/Services/Platform/PlatformEventService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq.Expressions;
 using TuPencaUy.Core.DataAccessLogic;
 using TuPencaUy.Core.DTOs;
+using TuPencaUy.Core.Enums;
 using TuPencaUy.Core.Exceptions;
 using TuPencaUy.Platform.DAO.Models;
 
@@ -22,6 +23,322 @@ namespace TuPencaUy.Core.DataServices.Services.Platform
       _sportDAL = sportDAL;
       _teamDAL = teamDAL;
       _matchDAL = matchDAL;
+    }
+    public MatchDTO ModifyMatch(
+      int idMatch,
+      int? idFirstTeam,
+      int? idSecondTeam,
+      DateTime? date,
+      int? firstTeamScore,
+      int? secondTeamScore,
+      int? sportId)
+    {
+      var originMatch = _matchDAL.Get(new List<Expression<Func<Match, bool>>> { x => x.Id == idMatch })
+        .FirstOrDefault() ?? throw new MatchNotFoundException($"Match with id {idMatch} not found");
+
+      bool modified = false;
+
+      if (idFirstTeam != null && idFirstTeam != originMatch.FirstTeam_id)
+      {
+        originMatch.FirstTeam = _teamDAL.Get(new List<Expression<Func<Team, bool>>> { x => x.Id == idFirstTeam })
+          .FirstOrDefault() ?? throw new TeamNotFoundException($"Team with id {idFirstTeam} not found");
+        modified = modified || true;
+      }
+
+      if (idSecondTeam != null && idSecondTeam != originMatch.SecondTeam_id)
+      {
+        originMatch.SecondTeam = _teamDAL.Get(new List<Expression<Func<Team, bool>>> { x => x.Id == idSecondTeam })
+          .FirstOrDefault() ?? throw new TeamNotFoundException($"Team with id {idSecondTeam} not found");
+        modified = modified || true;
+      }
+
+      if (date != null && date != originMatch.Date)
+      {
+        originMatch.Date = date;
+        modified = modified || true;
+      }
+
+      if (firstTeamScore != null && firstTeamScore != originMatch.FirstTeamScore)
+      {
+        originMatch.FirstTeamScore = firstTeamScore;
+        modified = modified || true;
+      }
+
+      if (secondTeamScore != null && secondTeamScore != originMatch.SecondTeamScore)
+      {
+        originMatch.SecondTeamScore = secondTeamScore;
+        modified = modified || true;
+      }
+
+      if (sportId != null && sportId != originMatch.Sport_id)
+      {
+        originMatch.Sport = _sportDAL.Get(new List<Expression<Func<Sport, bool>>> { x => x.Id == sportId })
+          .FirstOrDefault() ?? throw new SportNotFoundException($"Sport with id {sportId} not found");
+        modified = modified || true;
+      }
+
+      if (modified)
+      {
+        _matchDAL.Update(originMatch);
+        _matchDAL.SaveChanges();
+      }
+
+      return new MatchDTO
+      {
+        Id = idMatch,
+        FirstTeam = originMatch.FirstTeam != null ? new TeamDTO
+        {
+          Id = originMatch.FirstTeam.Id,
+          Name = originMatch.FirstTeam.Name,
+          TeamType = originMatch.FirstTeam.TeamType,
+          Sport = originMatch.FirstTeam.Sport != null ? new SportDTO
+          {
+            Id = originMatch.FirstTeam.Sport.Id,
+            Name = originMatch.FirstTeam.Sport.Name,
+            ExactPoints = originMatch.FirstTeam.Sport.ExactPoints,
+            Tie = originMatch.FirstTeam.Sport.Tie,
+            PartialPoints = originMatch.FirstTeam.Sport.PartialPoints,
+          } : null,
+          Logo = originMatch.FirstTeam.Logo,
+        } : null,
+        SecondTeam = originMatch.SecondTeam != null ? new TeamDTO
+        {
+          Id = originMatch.SecondTeam.Id,
+          Name = originMatch.SecondTeam.Name,
+          TeamType = originMatch.SecondTeam.TeamType,
+          Sport = originMatch.SecondTeam.Sport != null ? new SportDTO
+          {
+            Id = originMatch.SecondTeam.Sport.Id,
+            Name = originMatch.SecondTeam.Sport.Name,
+            ExactPoints = originMatch.SecondTeam.Sport.ExactPoints,
+            Tie = originMatch.SecondTeam.Sport.Tie,
+            PartialPoints = originMatch.SecondTeam.Sport.PartialPoints,
+          } : null,
+          Logo = originMatch.SecondTeam.Logo,
+        } : null,
+        FirstTeamScore = originMatch.FirstTeamScore,
+        SecondTeamScore = originMatch.SecondTeamScore,
+        Date = originMatch.Date,
+        Sport = originMatch.Sport != null ? new SportDTO
+        {
+          Id = originMatch.Sport.Id,
+          Name = originMatch.Sport.Name,
+          ExactPoints = originMatch.Sport.ExactPoints,
+          Tie = originMatch.Sport.Tie,
+          PartialPoints = originMatch.Sport.PartialPoints,
+        } : null,
+      };
+    }
+    public TeamDTO ModifyTeam(
+      int idTeam,
+      string? name,
+      byte[]? logo,
+      TeamTypeEnum? teamType,
+      int? sportId)
+    {
+      var originTeam = _teamDAL.Get(new List<Expression<Func<Team, bool>>> { x => x.Id == idTeam})
+        .FirstOrDefault() ?? throw new TeamNotFoundException($"Team with id {idTeam} not found");
+
+      bool modified = false;
+
+      if(name != null && name != originTeam.Name)
+      {
+        originTeam.Name = name;
+        modified = modified || true;
+      }
+
+      if (logo != null && logo != originTeam.Logo)
+      {
+        originTeam.Logo = logo;
+        modified = modified || true;
+      }
+
+      if (teamType != null && teamType != originTeam.TeamType)
+      {
+        originTeam.TeamType = teamType;
+        modified = modified || true;
+      }
+
+      if (sportId != null && sportId != originTeam.Sport_id)
+      {
+        originTeam.Sport = _sportDAL.Get(new List<Expression<Func<Sport, bool>>> { x => x.Id == sportId})
+          .FirstOrDefault() ?? throw new SportNotFoundException($"Sport with id {sportId} not found");
+        modified = modified || true;
+      }
+
+      if (modified)
+      {
+        _teamDAL.Update(originTeam);
+        _teamDAL.SaveChanges();
+      }
+
+      return new TeamDTO
+      {
+        Id = idTeam,
+        Name = originTeam.Name,
+        TeamType = originTeam.TeamType,
+        Sport = originTeam.Sport != null ? new SportDTO
+        {
+          Id = originTeam.Sport.Id,
+          Name = originTeam.Sport.Name,
+          ExactPoints = originTeam.Sport.ExactPoints,
+          Tie = originTeam.Sport.Tie,
+          PartialPoints = originTeam.Sport.PartialPoints,
+        } : null,
+        Logo = originTeam.Logo,
+      };
+    }
+    public SportDTO ModifySport(
+      int idSport,
+      string? name,
+      bool? tie,
+      int? exactPoints,
+      int? partialPoints)
+    {
+      var originSport = _sportDAL.Get(new List<Expression<Func<Sport, bool>>> { x => x.Id == idSport })
+        .FirstOrDefault() ?? throw new SportNotFoundException($"Sport with id {idSport} not found");
+
+      bool modified = false;
+
+      if(name != null && name != originSport.Name)
+      {
+        originSport.Name = name;
+        modified = modified || true;
+      }
+
+      if (tie != null && tie != originSport.Tie)
+      {
+        originSport.Tie = tie.Value;
+        modified = modified || true;
+      }
+
+      if (exactPoints != null && exactPoints != originSport.ExactPoints)
+      {
+        originSport.ExactPoints = exactPoints;
+        modified = modified || true;
+      }
+
+      if (partialPoints != null && partialPoints != originSport.PartialPoints)
+      {
+        originSport.PartialPoints = partialPoints;
+        modified = modified || true;
+      }
+
+      if (modified)
+      {
+        _sportDAL.Update(originSport);
+        _sportDAL.SaveChanges();
+      }
+
+      return new SportDTO
+      {
+        Id = originSport.Id,
+        Name = originSport.Name,
+        ExactPoints = originSport.ExactPoints,
+        Tie = originSport.Tie,
+        PartialPoints = originSport.PartialPoints,
+      };
+    }
+    public EventDTO ModifyEvent(
+      int idEvent,
+      string? name,
+      DateTime? startDate,
+      DateTime? endTime,
+      float? comission,
+      TeamTypeEnum? teamType)
+    {
+      var originEvent = _eventDAL.Get(new List<Expression<Func<Event, bool>>> { x => x.Id == idEvent })
+        .FirstOrDefault() ?? throw new EventNotFoundException($"Event with id {idEvent} not found");
+
+      bool modified = false;
+
+      if(name != null && name != originEvent.Name)
+      {
+        originEvent.Name = name;
+        modified = modified || true;
+      }
+
+      if (startDate != null && startDate != originEvent.StartDate)
+      {
+        originEvent.StartDate = startDate;
+        modified = modified || true;
+      }
+
+      if (endTime != null && endTime != originEvent.EndDate)
+      {
+        originEvent.EndDate = endTime;
+        modified = modified || true;
+      }
+
+      if (comission != null && comission != originEvent.Comission)
+      {
+        originEvent.Comission = comission;
+        modified = modified || true;
+      }
+
+      if (teamType != null && teamType != originEvent.TeamType)
+      {
+        originEvent.TeamType = teamType;
+        modified = modified || true;
+      }
+
+      if (modified)
+      {
+        _eventDAL.Update(originEvent);
+        _eventDAL.SaveChanges();
+      }
+
+      return new EventDTO
+      {
+        Id = originEvent.Id,
+        Name = originEvent.Name,
+        EndDate = originEvent.EndDate,
+        Comission = originEvent.Comission,
+        StartDate = originEvent.StartDate,
+        TeamType = originEvent.TeamType,
+      };
+    }
+    public void DeleteMatch(int idMatch)
+    {
+      var match = _matchDAL.Get(new List<Expression<Func<Match, bool>>> { match => match.Id == idMatch })
+        .FirstOrDefault() ?? throw new MatchNotFoundException($"Match with id {idMatch} not found");
+
+      _matchDAL.Delete(match);
+      _matchDAL.SaveChanges();
+    }
+    public void DeleteTeam(int idTeam)
+    {
+      var team = _teamDAL.Get(new List<Expression<Func<Team, bool>>> { team => team.Id == idTeam })
+        .FirstOrDefault() ?? throw new TeamNotFoundException($"Team with id {idTeam} not found");
+
+      _teamDAL.Delete(team);
+      _teamDAL.SaveChanges();
+    }
+    public void DeleteSport(int idSport)
+    {
+      var sport = _sportDAL.Get(new List<Expression<Func<Sport, bool>>> { sport => sport.Id == idSport })
+        .FirstOrDefault() ?? throw new SportNotFoundException($"Sport with id {idSport} not found");
+
+      _sportDAL.Delete(sport);
+      _sportDAL.SaveChanges();
+    }
+    public void DeleteEvent(int idEvent)
+    {
+      var ev = _eventDAL.Get(new List<Expression<Func<Event, bool>>> { ev => ev.Id == idEvent })
+        .FirstOrDefault() ?? throw new SportNotFoundException($"Event with id {idEvent} not found");
+
+      var matches = _eventDAL.Get(new List<Expression<Func<Event, bool>>> { ev => ev.Id == idEvent }).Select(x => x.Matches).FirstOrDefault();
+
+      if (matches != null && matches.Any())
+      {
+        foreach (var match in matches)
+        {
+          _eventDAL.Delete(match.Id);
+        }
+      }
+
+      _eventDAL.Delete(ev);
+      _eventDAL.SaveChanges();
     }
     public MatchDTO GetMatch(int idMatch)
     {
