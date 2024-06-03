0f4c64e629ee0ce20b41890f984dd1f796c5ad60
Gastón <gastonsomac@gmail.com>
2024-06-03 16:52:00 -0300

    Web controller layer

---
+++ TuPencaUy.CoreAPI/TuPencaUy.CoreAPI/Model/Requests/ModifyTeamRequest.cs
@@ -0,0 +1,12 @@
+﻿using TuPencaUy.Core.Enums;
+
+namespace TuPencaUy.Core.API.Model.Requests
+{
+  public class ModifyTeamRequest
+  {
+    public string? Name { get; set; }
+    public byte[]? Logo { get; set; }
+    public TeamTypeEnum? TeamType { get; set; }
+    public int? Sport { get; set; }
+  }
+}
