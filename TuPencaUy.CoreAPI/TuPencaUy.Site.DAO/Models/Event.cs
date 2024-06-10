af0148b37393191ea3c35de7e54a3e1bb8c5fd1e
Gastón <gastonsomac@gmail.com>
2024-06-10 20:40:46 -0300

    Off branch: preparing site context to referece central entities

--- TuPencaUy.CoreAPI/TuPencaUy.Site.DAO/Models/Event.cs
+++ TuPencaUy.CoreAPI/TuPencaUy.Site.DAO/Models/Event.cs
@@ -7,5 +7,6 @@ namespace TuPencaUy.Site.DAO.Models
     public virtual ICollection<Sport>? Sports { get; set; }
     public virtual ICollection<Match>? Matches { get; set; }
     public ICollection<User>? Users { get; set; }
+    public int RefEvent { get; set; }
   }
 }
