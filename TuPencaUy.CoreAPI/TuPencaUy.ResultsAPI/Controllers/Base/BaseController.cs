d075c0852aacf4ca59c819e10a084403e0cc0eda
Gastón <gastonsomac@gmail.com>
2024-06-19 18:08:53 -0300

    Added a results API with endpoints for matches and event

---
+++ TuPencaUy.CoreAPI/TuPencaUy.ResultsAPI/Controllers/Base/BaseController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using TuPencaUy.Core.Exceptions;
+using TuPencaUy.ResultsAPI.Model;
+
+namespace TuPencaUy.ResultsAPI.Controllers.Base
+{
+  public class BaseController : Controller
+  {
+    protected GameStatusEnum CalculateGameStatus(DateTime? startTime)
+    {
+      if (!startTime.HasValue) return GameStatusEnum.Uknown;
+
+      if (DateTime.Now < startTime.Value) return GameStatusEnum.Pending;
+      if (DateTime.Now < startTime.Value.AddMinutes(150)) return GameStatusEnum.InProgress; // Prudential time
+      return GameStatusEnum.Finalized;
+    }
+    protected IActionResult ManageException(Exception ex)
+    {
+      var errorResponse = new ApiResponse();
+
+      if (ex is NotFoundException)
+      {
+        errorResponse.Error = true;
+        errorResponse.Message = ex.Message;
+        return StatusCode((int)HttpStatusCode.NotFound, errorResponse);
+      }
+
+      if (ex is BadRequestException)
+      {
+        errorResponse.Error = true;
+        errorResponse.Message = ex.Message;
+        return StatusCode((int)HttpStatusCode.BadRequest, errorResponse);
+      }
+
+      errorResponse.Error = true;
+      errorResponse.Message = "An internal error has occurred";
+      return StatusCode((int)HttpStatusCode.InternalServerError, errorResponse);
+    }
+  }
+}
---
+++ TuPencaUy.CoreAPI/TuPencaUy.ResultsAPI/Model/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TuPencaUy.ResultsAPI.Model
+{
+  public class ApiResponse
+  {
+    public bool Error { get; set; } = false;
+    public dynamic? Data { get; set; }
+    public string? Message { get; set; }
+  }
+}
