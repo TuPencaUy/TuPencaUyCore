baea6de85c008c2756cb0ce389e16ba1889a1dc1
Gastón <gastonsomac@gmail.com>
2024-06-19 19:39:26 -0300

    Added request not found exception

---
+++ TuPencaUy.CoreAPI/TuPencaUy.Exceptions/AccessRequestNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace TuPencaUy.Core.Exceptions
+{
+  public class AccessRequestNotFoundException : NotFoundException
+  {
+    public AccessRequestNotFoundException(string message) : base(message) { }
+    public AccessRequestNotFoundException() : base(message: "Access request not found") { }
+  }
+}
