ea7893e450a89f071f871b0fd546d1f6d1c33ef3
Gastón <gastonsomac@gmail.com>
2024-06-19 19:39:43 -0300

    Added requestDTO

---
+++ TuPencaUy.CoreAPI/TuPencaUy.DTOs/AccessRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using TuPencaUy.Core.Enums;
+using TuPencaUy.DTOs;
+
+namespace TuPencaUy.Core.DTOs
+{
+  public class AccessRequestDTO
+  {
+    public UserDTO? User { get; set; }
+    public AccessStatusEnum? AccessStatusCode { get; set; }
+    public string? AccessStatus { get; set; }
+
+    public DateTime? RequestTime { get; set; }  
+  }
+}
