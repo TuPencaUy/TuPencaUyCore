1df04db93fe030edc9de1e1fa96b2aa27c8c852f
Gastón <gastonsomac@gmail.com>
2024-06-18 21:43:54 -0300

    Added BetDTO

---
+++ TuPencaUy.CoreAPI/TuPencaUy.DTOs/BetDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuPencaUy.DTOs;
+
+namespace TuPencaUy.Core.DTOs
+{
+  public class BetDTO
+  {
+    public MatchDTO Match { get; set; }
+    public EventDTO Event { get; set; }
+    public UserDTO User { get; set; }
+    public int? ScoreFirstTeam { get; set; }
+    public int? ScoreSecondTeam { get; set; }
+    public int? Points { get; set; }
+  }
+}
