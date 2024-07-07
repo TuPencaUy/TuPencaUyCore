578238bb484dc5f5fe772098ba07cbce1324978f
Gastón <gastonsomac@gmail.com>
2024-07-07 14:50:54 -0300

    Added analytics controller struct with basic endpoints(payments analyticis will be added later)

---
+++ TuPencaUy.CoreAPI/TuPencaUy.CoreAPI/Controllers/AnalyticsController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using TuPencaUy.Core.DataServices;
+using TuPencaUy.CoreAPI.Controllers.Base;
+
+namespace TuPencaUy.Core.API.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class AnalyticsController : BaseController
+  {
+    private readonly IServiceFactory _serviceFactory;
+
+    public AnalyticsController(IServiceFactory serviceFactory)
+    {
+      _serviceFactory = serviceFactory;
+    }
+
+    [HttpGet("PositionsTable/{eventId}")]
+    public IActionResult GetPositionsTable([Required] int eventId, [FromQuery] int? page, [FromQuery] int? pageSize)
+    {
+      try
+      {
+        return Ok();
+      }
+      catch(Exception ex)
+      {
+        return ManageException(ex);
+      }
+    }
+
+    [HttpGet("Bets/Matches")]
+    public IActionResult GetMatchBets([FromQuery] int? matchId)
+    {
+      try
+      {
+        return Ok();
+      }
+      catch (Exception ex)
+      {
+        return ManageException(ex);
+      }
+    }
+
+    [HttpGet("Bets/Users")]
+    public IActionResult GetBetsUsers([FromQuery] int? matchId, [FromQuery] int? eventId)
+    {
+      try
+      {
+        return Ok();
+      }
+      catch (Exception ex)
+      {
+        return ManageException(ex);
+      }
+    }
+
+    [HttpGet("Bets/Events")]
+    public IActionResult GetEventBets([FromQuery] int? eventId)
+    {
+      try
+      {
+        return Ok();
+      }
+      catch (Exception ex)
+      {
+        return ManageException(ex);
+      }
+    }
+  }
+}
