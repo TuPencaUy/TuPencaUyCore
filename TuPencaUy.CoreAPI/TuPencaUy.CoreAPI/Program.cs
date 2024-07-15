a9dddc9ebf46cd997f0d9536d05484f7c934c395
Gastón <gastonsomac@gmail.com>
2024-07-15 02:05:51 -0300

    change swagger to release env

--- TuPencaUy.CoreAPI/TuPencaUy.CoreAPI/Program.cs
+++ TuPencaUy.CoreAPI/TuPencaUy.CoreAPI/Program.cs
@@ -61,16 +61,14 @@ var app = builder.Build();
 app.UseMiddleware<RequestContentMiddleware>();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-  app.UseSwagger();
-  app.UseSwaggerUI();
-  app.UseCors(builder => builder
-    .AllowAnyOrigin()
-    .AllowAnyMethod()
-    .AllowAnyHeader()
-  );
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors(builder => builder
+  .AllowAnyOrigin()
+  .AllowAnyMethod()
+  .AllowAnyHeader()
+);
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
